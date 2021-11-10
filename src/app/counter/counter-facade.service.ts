a440a9c3f19bd733b0dc4d52d864b37ba3aeff4d
ParthSSoni <parthsoni00001@gmail.com>
2021-11-10 11:14:12 +0530

    1 file modified, 1 deleted

--- src/app/counter/counter-facade.service.ts
+++ src/app/counter/counter-facade.service.ts
@@ -13,7 +13,6 @@ import {
 } from 'rxjs/operators';
 import { CounterStateKeys } from '../counter-state-keys.enum';
 import { CounterState } from '../counter-state.interface';
-import { INITIAL_COUNTER_STATE } from '../initial-counter-state';
 import { inputToValue } from '../operators/inputToValue';
 import { selectDistinctState } from '../operators/selectDistinctState';
 import { Command } from './command.interface';
@@ -23,7 +22,13 @@ import { Command } from './command.interface';
 })
 export class CounterFacadeService implements OnDestroy {
   // = CONSTANTS ============================================================
-  initialCounterState: CounterState = INITIAL_COUNTER_STATE;
+  initialCounterState: CounterState = {
+    isTicking: false,
+    count: 0,
+    countUp: true,
+    tickSpeed: 200,
+    countDiff: 1,
+  };
   ngOnDestroySubject = new Subject();
 
   // = BASE OBSERVABLES  ====================================================
--- src/app/initial-counter-state.ts
+++
@@ -1,7 +0,0 @@
-export const INITIAL_COUNTER_STATE = {
-  isTicking: false,
-  count: 0,
-  countUp: true,
-  tickSpeed: 200,
-  countDiff: 1,
-};
