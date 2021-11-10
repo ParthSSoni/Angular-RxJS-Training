a8cce1cb1678bbb51d592f912231a13549abea4e
ParthSSoni <parthsoni00001@gmail.com>
2021-11-10 10:33:01 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {RouterModule} from '@angular/router';
-import {APP_ROUTES} from './app-routing.module';
+import { RouterModule } from '@angular/router';
+import { APP_ROUTES } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
-import {ToArrayPipe} from './to-array.pipe';
+import { InputToValuePipe } from './input-to-value.pipe';
+import { ToArrayPipe } from './to-array.pipe';
+import { CounterViewComponent } from './counter/counter-view/counter-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CounterComponent,
-    ToArrayPipe
+    ToArrayPipe,
+    InputToValuePipe,
+    CounterViewComponent,
   ],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(APP_ROUTES),
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(APP_ROUTES)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
